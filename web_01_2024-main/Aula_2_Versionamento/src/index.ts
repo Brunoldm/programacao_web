56908caf85bf7fab57af7a3b7fce5313d553bb98
Brunoldm <bruno.ldm@hotmail.com>
2024-02-23 20:21:18 -0300

    Aula 3

--- web_01_2024-main/Aula_2_Versionamento/src/index.ts
+++ web_01_2024-main/Aula_2_Versionamento/src/index.ts
@@ -10,6 +10,7 @@ Vamos criar variáveis e objetos para representar diferentes tipos de dados rela
     email: do tipo string, representando o endereço de e-mail do usuário.
     isAtivo: do tipo boolean, representando se o usuário está ativo ou não.
 
+
 2 -Crie um objeto usuario com as seguintes propriedades:
 
     nome: string, representando o primeiro nome do usuário.
@@ -21,6 +22,38 @@ Vamos criar variáveis e objetos para representar diferentes tipos de dados rela
     Em seguida, imprima todas as variáveis e propriedades do objeto usuario usando console.log.
 
 */
+let usuario:{
+    nome: "Matheus",
+    sobrenome: "Silva",
+    idade: 19,
+    contato: {
+    email: "dasilvasantosmatheus504@gmail.com",
+    telefone:"15997448945",
+    }
+    }
+    type Contato = {
+    email: string,
+    telefone:string,
+    }
+    
+    type Pessoa = {
+    nome: string,
+    sobrenome: string,
+    idade: number,
+    contato: Contato,
+    }
+    
+    let user: Pessoa;
+    user = {
+    nome: "Matheus",
+    sobrenome: "Silva",
+    idade: 19,
+    contato: {
+    email: "dasilvasantosmatheus504@gmail.com",
+    telefone:"15997448945",
+    }
+    }
+    console.log(usuario,user);
 
 
 /*
@@ -35,7 +68,16 @@ Precisamos armazenar informações sobre diferentes produtos disponíveis na loj
     preco: do tipo number, representando o preço do produto.
     disponivel: do tipo boolean, representando se o produto está disponível em estoque ou não.
 
+    */
+    let produto: {
+        nome: string,
+        preco: number,
+        estoque: number,
+        categorias: string,
+        
 
+
+/*
 02 - Crie um objeto produto com as seguintes propriedades:
 
     nome: string, representando o nome do produto.
