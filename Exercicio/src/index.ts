4b2f43d7a25247666de5b63980b1d2d3dd9fd0dc
brunoldm <bruno.ldm@hotmail.com>
2024-03-01 19:38:52 -0300

    teste

--- Exercicio/src/index.ts
+++ Exercicio/src/index.ts
@@ -2,7 +2,7 @@ function lista(numero01 : number, numero02: number, numero03: number, numero04: 
     let resultado;
     switch (operacao) {
         case "soma": {
-             max(numero01,numero02,numero03,numero04);
+             max(numero01,numero02,numero03,numero04);fff
 
 }
 }
