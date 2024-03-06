f33e943168567a2c12548abb19d223b2737310ab
Brunoldm <bruno.ldm@hotmail.com>
2024-03-06 00:08:01 -0300

    Finalizando exercicios

--- Exercicio/src/exercicio.js
+++ Exercicio/src/exercicio.js
@@ -3,106 +3,79 @@
 Crie uma função que receba uma lista de números como parâmetro e retorne o
 maior número da lista.
 */
-/*function encontrarMaiorNumero(Numeros: number[]): number {
-
-    let maior = Numeros[0];
-    for (let i = 1; i < Numeros.length; i++) {
+function encontrarMaiorNumero(Numeros) {
+    var maior = Numeros[0];
+    for (var i = 1; i < Numeros.length; i++) {
         if (Numeros[i] > maior) {
             maior = Numeros[i];
         }
     }
-
     return maior;
 }
-
-let numeros01: number[] = [5 , 22 , 7 , 13 , 8 , 3];
-let maiorNumero = encontrarMaiorNumero(numeros01);
+var numeros01 = [5, 22, 7, 13, 8, 3];
+var maiorNumero = encontrarMaiorNumero(numeros01);
 console.log('O maior número é:', maiorNumero);
-*/
 /*Exercício 2
 
 Desenvolva uma função que receba um número como parâmetro e verifique se
 ele é par ou ímpar. Retorne true se for par e false se for ímpar.
 */
-/*function teste(valor: number): string {
-
-    if(valor % 2 === 0){
-
+function teste(valor) {
+    if (valor % 2 === 0) {
         return "Par";
     }
-    else{
+    else {
         return "Impar";
     }
 }
-console.log('o numero é',teste(9));
-*/
+console.log('o numero é', teste(9));
 /*
 Exercício 3
 
 Implemente uma função que calcule a média aritmética de um array de números
 e retorne o resultado. Utilize essa função para calcular a média de diferentes
 conjuntos de números.
-
-
-function media(valores: number[]): number{
-    
-    let soma = 0;
-
-        for (let i = 0; i < valores.length; i++) {
-            
-            soma += valores[i];
-        }
-        
-     let resultadodamedia = soma/valores.length;
-
-    return resultadodamedia;
-    }
-
-let notas: number[] = [5 , 7 , 9 , 10 , 8 , 3];
-let resultado = media(notas);
-console.log('O resultado da média é:', resultado);
-
 */
+function media(valores) {
+    var soma = 0;
+    for (var i = 0; i < valores.length; i++) {
+        soma += valores[i];
+    }
+    var resultadodamedia = soma / valores.length;
+    return resultadodamedia;
+}
+var notas = [5, 7, 9, 10, 8, 3];
+var resultado = media(notas);
+console.log('O resultado da média é:', resultado);
 /*
 Exercício 4
 
 Crie uma função que receba uma string como parâmetro e retorne a mesma
 string com todas as letras em caixa alta. Utilize essa função para converter
 diferentes strings.
-
-
-function converterparamaiuscula(texto: string): string {
-
-    return texto.toUpperCase();
-
-}
-
-console.log(converterparamaiuscula("Valeu professor"));
 */
+function converterparamaiuscula(texto) {
+    return texto.toUpperCase();
+}
+console.log(converterparamaiuscula("Valeu professor"));
 /*
 Exercício 5
 
 Desenvolva uma função que determine se um número é primo ou não. Retorne
 true se for primo e false se não for.
-
-
-function primo(numero: number): boolean{
-
-    if (numero <= 1){
+*/
+function primo(numero) {
+    if (numero <= 1) {
         return false;
     }
-        for(let i = 2; i <= Math.sqrt(numero); i++){
-        
-        if (numero % i === 0){
-
+    for (var i = 2; i <= Math.sqrt(numero); i++) {
+        if (numero % i === 0) {
             return false;
         }
     }
-        return true;
+    return true;
 }
-
 console.log(primo(7));
-*/
 /*
 Exercício 6
 
@@ -114,3 +87,55 @@ function inverterArray(array) {
 }
 var array1 = ['B', 'E', 'X', 'I', 'G', 'A'];
 console.log(inverterArray(array1));
+/*
+Exercício 7
+
+Crie uma função que receba um valor e uma porcentagem como parâmetros. A
+função deve retornar o valor acrescido da porcentagem indicada.
+*/
+function acrescentarPorcentagem(valor, porcentagem) {
+    var valorAcrescido = valor * (1 + porcentagem / 100);
+    return valorAcrescido;
+}
+var valorInicial = 20;
+var porcentagemAcrescimo = 10;
+var novoValor = acrescentarPorcentagem(valorInicial, porcentagemAcrescimo);
+console.log(novoValor);
+/*
+Exercício 8
+
+Crie uma função que receba uma string e retorne a mesma string, mas com as
+palavras em ordem reversa.
+*/
+function ordemreversa(frase) {
+    var separar = frase.split(" ");
+    var inverter = separar.reverse();
+    var juntar = inverter.join(" ");
+    return juntar;
+}
+console.log(ordemreversa("Preciso de um feriado"));
+/*
+Exercício 9
+
+Implemente uma função que retorne a soma de todos os números pares em um
+array.
+*/
+function somarnumerospares(numeros) {
+    var soma = 0;
+    for (var _i = 0, numeros_1 = numeros; _i < numeros_1.length; _i++) {
+        var sequencia = numeros_1[_i];
+        if (sequencia % 2 === 0) {
+            soma += sequencia;
+        }
+    }
+    return soma;
+}
+var numeros03 = [1, 5, 2, 7, 8, 20, 32, 33];
+var somadospares = somarnumerospares(numeros03);
+console.log("A soma dos números pares é:", somadospares);
+/*
+Exercício 10
+
+Crie uma função que calcule o fatorial de um número. Utilize essa função para
+calcular o fatorial de diferentes números.
+*/
--- Exercicio/src/exercicio.ts
+++ Exercicio/src/exercicio.ts
@@ -4,7 +4,7 @@ Crie uma função que receba uma lista de números como parâmetro e retorne o
 maior número da lista. 
 */
 
-/*function encontrarMaiorNumero(Numeros: number[]): number {
+function encontrarMaiorNumero(Numeros: number[]): number {
 
     let maior = Numeros[0];
     for (let i = 1; i < Numeros.length; i++) {
@@ -19,7 +19,7 @@ maior número da lista.
 let numeros01: number[] = [5 , 22 , 7 , 13 , 8 , 3];
 let maiorNumero = encontrarMaiorNumero(numeros01);
 console.log('O maior número é:', maiorNumero); 
-*/
+
 
 /*Exercício 2
 
@@ -27,7 +27,7 @@ Desenvolva uma função que receba um número como parâmetro e verifique se
 ele é par ou ímpar. Retorne true se for par e false se for ímpar.
 */
 
-/*function teste(valor: number): string {
+function teste(valor: number): string {
 
     if(valor % 2 === 0){
 
@@ -38,7 +38,7 @@ ele é par ou ímpar. Retorne true se for par e false se for ímpar.
     }   
 }
 console.log('o numero é',teste(9));
-*/
+
 
 /*
 Exercício 3
@@ -46,7 +46,7 @@ Exercício 3
 Implemente uma função que calcule a média aritmética de um array de números
 e retorne o resultado. Utilize essa função para calcular a média de diferentes
 conjuntos de números.
-
+*/
 
 function media(valores: number[]): number{
     
@@ -66,14 +66,14 @@ let notas: number[] = [5 , 7 , 9 , 10 , 8 , 3];
 let resultado = media(notas);
 console.log('O resultado da média é:', resultado); 
 
-*/
+
 /*
 Exercício 4
 
 Crie uma função que receba uma string como parâmetro e retorne a mesma
 string com todas as letras em caixa alta. Utilize essa função para converter
 diferentes strings.
-
+*/
 
 function converterparamaiuscula(texto: string): string {
 
@@ -82,13 +82,13 @@ function converterparamaiuscula(texto: string): string {
 }
 
 console.log(converterparamaiuscula("Valeu professor"));
-*/
+
 /*
 Exercício 5
 
 Desenvolva uma função que determine se um número é primo ou não. Retorne
 true se for primo e false se não for.
-
+*/
 
 function primo(numero: number): boolean{
 
@@ -106,13 +106,13 @@ function primo(numero: number): boolean{
 }
 
 console.log(primo(7));
-*/
+
 /*
 Exercício 6
 
 Implemente uma função que inverta a ordem dos elementos em um array.
 Utilize essa função para inverter a ordem de diferentes conjuntos de elementos.
-
+*/
 
 function inverterArray<T>(array: T[]): T[] {
     return array.reverse();
@@ -120,7 +120,7 @@ function inverterArray<T>(array: T[]): T[] {
 
 let array1 = ['B' ,'E', 'X', 'I', 'G', 'A'];
 console.log(inverterArray(array1));
-*/
+
 /*
 Exercício 7
 
@@ -129,12 +129,59 @@ função deve retornar o valor acrescido da porcentagem indicada.
 */
 
 function acrescentarPorcentagem(valor: number, porcentagem: number): number {
-    const valorAcrescido = valor * (1 + porcentagem / 100);
+    let valorAcrescido = valor * (1 + porcentagem / 100);
     return valorAcrescido;
 }
 
-const valorInicial = 100;
-const porcentagemAcrescimo = 10;
+let valorInicial = 20;
+let porcentagemAcrescimo = 10;
 
-const novoValor = acrescentarPorcentagem(valorInicial, porcentagemAcrescimo);
+let novoValor = acrescentarPorcentagem(valorInicial, porcentagemAcrescimo);
 console.log(novoValor);
+
+/*
+Exercício 8
+
+Crie uma função que receba uma string e retorne a mesma string, mas com as
+palavras em ordem reversa.
+*/
+
+function ordemreversa(frase: string): string{
+
+    let separar = frase.split(" ");
+    let inverter = separar.reverse();
+    let juntar = inverter.join(" ");
+
+    return juntar;
+}
+
+console.log(ordemreversa("Preciso de um feriado"));
+
+/*
+Exercício 9
+
+Implemente uma função que retorne a soma de todos os números pares em um
+array.
+*/
+
+function somarnumerospares(numeros: number[]): number {
+    let soma = 0;
+    for (let sequencia of numeros) {
+        if (sequencia % 2 === 0) { 
+
+            soma += sequencia;
+        }
+    }
+    return soma;
+}
+
+const numeros03 = [1, 5, 2, 7, 8, 20, 32, 33];
+const somadospares = somarnumerospares(numeros03);
+console.log("A soma dos números pares é:", somadospares);
+
+/*
+Exercício 10
+
+Crie uma função que calcule o fatorial de um número. Utilize essa função para
+calcular o fatorial de diferentes números.
+*/
