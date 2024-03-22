e5293aea2f7c36d571274c845191046ebed759e1
Brunoldm <bruno.ldm@hotmail.com>
2024-03-22 20:23:32 -0300

    Aula 10

---
+++ Aula_09/src/model/produtos.ts
@@ -0,0 +1,11 @@
+class produto {
+    nome : string;
+    descricao: string;
+    valor: number;
+
+    constructor(nome: string,descricao:string,valor:number){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+}
